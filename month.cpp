37fec969fcf80d833c84cf2c7f07c3f56b72e4d3
Jon <jongulrich@gmail.com>
2020-08-31 21:23:09 -0400

    Month tester

--- month.cpp
+++ month.cpp
@@ -1,15 +1,108 @@
 #include <stdio.h>
 #include <iostream>
+#include <map>
 
 using namespace std;
+
+map<int, int> months = 
+{
+	{1,31},
+	{2,28},
+	{3,31},
+	{4,30},
+	{5,31},
+	{6,30},
+	{7,31},
+	{8,31},
+	{9,30},
+	{10,31},
+	{11,30},
+	{12,31}
+};
+
+int getNextMonth(int curr)
+{
+	int next = curr + 1;
+	if (next > 12)
+		return 1;
+	else
+		return next;
+}
+
+int days(int month)
+{
+	if (month <= 12 && month >= 1)
+		return months[month];
+	else
+		return -1;
+}
+
+typedef struct Date
+{
+	int day;
+	int month;
+	int year;
+
+} Date;
+ostream& operator << (ostream &os, const Date &date) {
+    return (os <<  date.month << "/" << date.day << "/" << date.year);
+}
+
+
+bool  greaterThanMonth(Date date1, Date date2)
+{
+	int diff = 0;
+	int date1total = date1.year*10000 + date1.month*100 + date1.day;
+	int date2total = date2.year*10000 + date2.month*100 + date2.day;
+	bool date1greater = (date1total > date2total);
+	
+	
+	if (date1greater) 
+		diff = date1total - date2total;
+	else
+		diff = date2total - date1total;
+		
+		
+	int onemonthbetween = 0;
+	if (date1greater)
+		onemonthbetween = days(date2.month); 
+	else
+		onemonthbetween = days(date1.month); 
+		
+		
+	if (diff > onemonthbetween)
+		return true;
+	else
+		return false;
+}
+
 int main (int argc, char** argv)
 {
-    int month1 = std::stoi(argv[1]);
-    int day1 = std::stoi(argv[2]);
-    int month2 = std::stoi(argv[3]);
-    int day2 = std::stoi(argv[4]);
+	bool leapyear = false;
+	int argvit = 0;
+	Date day1;
+	Date day2;
+		
+	if (argc > 5)
+	{
+
+     	day1.month  = stoi(argv[++argvit]);
+		day1.day = stoi(argv[++argvit]);
+		day1.year= stoi(argv[++argvit]);
+		
+		day2.month  = stoi(argv[++argvit]);
+		day2.day = stoi(argv[++argvit]);
+		day2.year= stoi(argv[++argvit]);
+		
+		string res = greaterThanMonth(day1,day2)?"":" NOT";
+    	cout << "Compare: " << day1 << " is" << res << " more than a month from " << day2 << endl;
+    }
+    else
+    {
+    	cout << "invalid entry: mm1 dd1 yyyy1 mm2 dd2 yyyy2" << endl;
+    }
+
     
-    cout << "Compare: " << month1 << "/" << day1 << " & " << month2 << "/" << day2 << endl;
     return 0;
 
 }
